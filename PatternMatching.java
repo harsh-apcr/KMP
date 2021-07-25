d5f1bec0b1bfd4cc21c52e8f82837c80a4e87b8d
harsh-apcr <harsh2001apcr@gmail.com>
2021-07-25 14:03:01 +0530

    bug fixed and test cases added

--- PatternMatching.java
+++ PatternMatching.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Random;
 
 public class PatternMatching {
 
@@ -46,16 +49,53 @@ public class PatternMatching {
             else {
                 if (j == m-1) {
                     patternOccurrences.add(i-m+1);
+                    j = prefixLength[j-1];
+                } else {
+                    i++;
+                    j++;
                 }
-                i++;
-                j++;
             }
         }
         return patternOccurrences;
     }
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final Random rd = new Random();
+        int n = Integer.parseInt(br.readLine());    // text length
+        int m = Integer.parseInt(br.readLine());    // pattern length
+        StringBuilder text = new StringBuilder();
+        for (int i = 0 ; i < n ;i++) {
+            text.append((char) (rd.nextInt(26)+'a'));
+        }
+        StringBuilder pattern = new StringBuilder();
+        for (int i = 0;i < m;i++) {
+            pattern.append((char)(rd.nextInt(26) + 'a'));
+        }
+        String text1 = String.valueOf(text);
+        String pattern1 = String.valueOf(pattern);
+        int idx = -1;
+        ArrayList<Integer> matchedIdx = subStringSearch(text1, pattern1);
+        try {
+            idx = matchedIdx.get(0);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(idx);
+        }
+        System.out.println("does text contains pattern p ? " + !(idx == -1));
+        if (idx!=-1) {
+            boolean hasBug = false;
+            for(int i : matchedIdx) {
+                String toMatch = text.substring(i,i+m);
+                if (!toMatch.equals(pattern1)) {
+                    System.out.println("pattern finding has a bug");
+                    System.out.println(toMatch);
+                    System.out.println(pattern1);
+                    hasBug = true;
+                    break;
+                }
+            }
+            System.out.println("does it have a bug ? " + hasBug);
+        }
     }
 
 }
